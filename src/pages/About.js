5c8e6abf57a93718d5fb39552106d8a9f554bf9a
onepullnixxa <briseno.chris@gmail.com>
2021-12-05 15:16:18 -0500

    updates about page with new picture

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,49 @@
 import React from "react";
+import AboutPic from "../images/icecreamselfie.jpg"
 
-function About () {
-    return(
-        <div className="about-body">
-            <p>Hi! My name is Chris and I'm a full-stack software engineer living in <a href="https://goo.gl/maps/dH6UKCcZhkNqSkvr8">{`{Ridgewood, Queens}`}</a>.</p> <br />
-            <p>I took an interest in computer science after reading Safiya Noble's <a href="https://en.wikipedia.org/wiki/Algorithms_of_Oppression">{`{Algorithms of Oppression}`}</a>. I was fascinated by the power of code and the impact it has on shaping our daily lives, specifically how programs can perpetuate and contribute to discriminatory and anti-Black bias in society. I decided thereafter to study basic HTML, CSS, and JavaScript in order to prepare myself to begin my journey in tech.</p> <br />
-            <p>In August of 2021, I decided to enroll in General Assembly's Software Engineering Immersive program in order to expand upon my programming skills. In that short time, I've learned </p>
-        </div>
-    )
+function About() {
+  return (
+    <div className="about-body">
+      <img
+        className="about-pic"
+        src={AboutPic}
+        height={400}
+        width={400}
+        alt="chris eating ice cream"
+      />
+      <p>
+        Hi! My name is Chris and I'm a full-stack software engineer living in{" "}
+        <a href="https://goo.gl/maps/dH6UKCcZhkNqSkvr8">{`{Ridgewood, Queens}`}</a>
+        .
+      </p>{" "}
+      <br />
+      <p>
+        I took an interest in computer science after reading Safiya Noble's{" "}
+        <a href="https://en.wikipedia.org/wiki/Algorithms_of_Oppression">{`{Algorithms of Oppression}`}</a>
+        . I was fascinated by the power of code and the impact it has on shaping
+        our daily lives, specifically how programs can perpetuate and contribute
+        to discriminatory and anti-Black bias in society. I wanted to play a
+        role in creating software that would push back against these prejudices
+        and use my skills towards projects tackling houselessness, poverty,
+        transphobia, homophobia, and sexism. I decided thereafter to study basic
+        HTML, CSS, and JavaScript in order to prepare myself to begin my journey
+        in tech.
+      </p>{" "}
+      <br />
+      <p>
+        In the fall of 2021 I decided to enroll into General Assembly's Software
+        Engineering Immersive Bootcamp to build upon some of the fundamental
+        skills I had previously learned. In that intensive three month program,
+        I picked up both front and backend skills in some of the following
+        languages:
+      </p>{" "}
+      <br />
+      <a href="https://reactjs.org/">{`{React}`}</a> <br />
+      <a href="https://rubyonrails.org/">{`{Ruby on Rails}`}</a> <br />
+      <a href="https://www.djangoproject.com/">{`{Python / Django}`}</a> <br />
+      <a href="https://jquery.com/">{`{jQuery}`}</a> <br />
+    </div>
+  );
 }
 
-export default About
+export default About;
