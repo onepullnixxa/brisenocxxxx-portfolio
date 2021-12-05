e1e973b0b5fc4a3890ea37cec90a69cd9f33f305
onepullnixxa <briseno.chris@gmail.com>
2021-12-04 19:35:54 -0500

    modifies about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,8 +3,8 @@ import React from "react";
 function About () {
     return(
         <div className="about-body">
-            <p>Hi! My name is Chris and I'm a full-stack software engineer living in Ridgewood, Queens.</p> <br />
-            <p>I took an interest in computer science after reading Safiya Noble's <a href="https://en.wikipedia.org/wiki/Algorithms_of_Oppression">{`{Algorithms of Oppression}`}</a>. I was fascinated by the power of code and the impact it can have on shaping our daily lives, specifically how programs can perpetuate and contribute to discriminatory and anti-Black bias in society. I decided thereafter to study basic HTML, CSS, and JavaScript in order to prepare myself to begin my journey in tech.</p> <br />
+            <p>Hi! My name is Chris and I'm a full-stack software engineer living in <a href="https://goo.gl/maps/dH6UKCcZhkNqSkvr8">{`{Ridgewood, Queens}`}</a>.</p> <br />
+            <p>I took an interest in computer science after reading Safiya Noble's <a href="https://en.wikipedia.org/wiki/Algorithms_of_Oppression">{`{Algorithms of Oppression}`}</a>. I was fascinated by the power of code and the impact it has on shaping our daily lives, specifically how programs can perpetuate and contribute to discriminatory and anti-Black bias in society. I decided thereafter to study basic HTML, CSS, and JavaScript in order to prepare myself to begin my journey in tech.</p> <br />
             <p>In August of 2021, I decided to enroll in General Assembly's Software Engineering Immersive program in order to expand upon my programming skills. In that short time, I've learned </p>
         </div>
     )
