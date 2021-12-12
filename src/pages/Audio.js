999b7e0d5a79741b64270d64dfa0699db18c88f1
onepullnixxa <briseno.chris@gmail.com>
2021-12-12 13:23:47 -0500

    adds dreamcast to audio page

--- src/pages/Audio.js
+++ src/pages/Audio.js
@@ -1,10 +1,21 @@
 import React from "react";
+import Dreamcast from "../images/dreamcast.jpg";
 import TenTrax from "../images/10trax.jpg";
 import NTSscreen from "../images/NTSscreen.jpg";
 
 function Audio() {
   return (
     <div className="audio-body">
+      <img
+        src={Dreamcast}
+        height={400}
+        width={750}
+        alt="screen shot of Relativa Radio page"
+      />
+      <p>
+        <a href="https://radiorelativa.eu/show/dreamcast/">{`{Dreamcast}`}</a> 
+        — An ongoing program with Madrid's Relativa Radio.
+      </p>
       <img
         src={TenTrax}
         height={400}
@@ -24,7 +35,8 @@ function Audio() {
       />
       <p>
         <a href="https://www.nts.live/shows/image-search/episodes/image-search-16-08-17">{`{Animal Image Search}`}</a>{" "}
-        — Served as a guest artist on an episode of Landon Odle's series on NTS Radio.
+        — Served as a guest artist on an episode of Landon Odle's series on NTS
+        Radio.
       </p>
     </div>
   );
