018315d1b6e443f9277a2fa486693f328db115c3
onepullnixxa <briseno.chris@gmail.com>
2022-01-22 19:17:26 -0500

    adjusts some alignment issues in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div>
       <header>
         <Link to="/">
-          <h4 id="home">Christopher Briseño</h4>
+          <h1 id="home">Christopher Briseño</h1>
         </Link>
       </header>
       <nav>
