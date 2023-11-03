02f061d1ea873882c7191454bfa0edddbb3ced99
onepullnixxa <briseno.chris@gmail.com>
2023-11-03 09:51:58 -0400

    adds big feelings project to projects.js

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,31 +5,48 @@ import BookExcerpts from "../images/excerpts-screenshot.jpg";
 import VarietyCoffee from "../images/variety-coffee.jpg";
 import DuneSuncare from "../images/dune-screenshot.jpg";
 import ParFemme from "../images/par-femme.jpg";
+import BigFeelings from "../images/big-feelings.png";
 
 function Projects() {
   return (
     <div className="projects-body">
       <img
-        src={ParFemme}
+        src={BigFeelings}
         height={400}
         width={750}
-        alt="par femme homepage"
+        alt="big feelings homepage"
       />
       <p>
-        <a href="https://www.parfemme.net"
-        target="_blank" rel="noreferrer">{`{Par Femme}`}</a>{" "}
+        <a
+          href="https://bigfeelings.com/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Big Feelings}`}</a>{" "}
+        — Built with Vue 3, Alpine.js, Tailwind CSS and Shopify Liquid
+      </p>
+      <img src={ParFemme} height={400} width={750} alt="par femme homepage" />
+      <p>
+        <a
+          href="https://www.parfemme.net"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Par Femme}`}</a>{" "}
         — Built with Vue 3, Nuxt 3, Tailwind CSS and Sanity.io
       </p>
-      <img
-        src={junedays}
-        height={400}
-        width={750}
-        alt="junedays home page"
-      />
+      <img src={junedays} height={400} width={750} alt="junedays home page" />
       <p>
-        <a href="https://junedays.com/"
-        target="_blank" rel="noreferrer">{`{junedays}`}</a>{" "}
-        — Website completed with <a href="http://www.human-nyc.com/" target="_blank" rel="noreferrer">{`{Human NYC}`}</a>. Built with Shopify Liquid.
+        <a
+          href="https://junedays.com/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{junedays}`}</a>{" "}
+        — Website completed with{" "}
+        <a
+          href="http://www.human-nyc.com/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Human NYC}`}</a>
+        . Built with Shopify Liquid.
       </p>
       <img
         src={DuneSuncare}
@@ -38,10 +55,18 @@ function Projects() {
         alt="dune suncare home page"
       />
       <p>
-        <a href="https://dunesuncare.com/"
-        target="_blank" rel="noreferrer">{`{Dune Suncare}`}</a>{" "}
-        — Custom Shopify theme written and completed with <a href="http://www.human-nyc.com/"
-        target="_blank" rel="noreferrer">{`{Human NYC}`}</a>.
+        <a
+          href="https://dunesuncare.com/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Dune Suncare}`}</a>{" "}
+        — Custom Shopify theme written and completed with{" "}
+        <a
+          href="http://www.human-nyc.com/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Human NYC}`}</a>
+        .
       </p>
       <img
         src={SunniesFace}
@@ -50,20 +75,39 @@ function Projects() {
         alt="home page of Sunnies Face"
       />
       <p>
-        <a href="https://sunniesface.com/"
-        target="_blank" rel="noreferrer">{`{Sunnies Face}`}</a>{" "}
-        — Custom Shopify theme written and completed with <a href="http://www.human-nyc.com/"
-        target="_blank" rel="noreferrer">{`{Human NYC}`}</a>.
+        <a
+          href="https://sunniesface.com/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Sunnies Face}`}</a>{" "}
+        — Custom Shopify theme written and completed with{" "}
+        <a
+          href="http://www.human-nyc.com/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Human NYC}`}</a>
+        .
       </p>
-      <img 
+      <img
         src={VarietyCoffee}
         height={400}
         width={750}
         alt="home page of Variety Coffee Roasters site"
       />
       <p>
-        <a href="https://varietycoffeeroasters.com/" target="_blank" rel="noreferrer">{`{Variety Coffee Roasters}`}</a>{" "}
-        — Website completed in collaboration with <a href="http://www.human-nyc.com/" target="_blank" rel="noreferrer">{`{Human NYC}`}</a> for Brooklyn's Variety Coffee Roasters redesign. Built with Shopify Liquid.
+        <a
+          href="https://varietycoffeeroasters.com/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Variety Coffee Roasters}`}</a>{" "}
+        — Website completed in collaboration with{" "}
+        <a
+          href="http://www.human-nyc.com/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Human NYC}`}</a>{" "}
+        for Brooklyn's Variety Coffee Roasters redesign. Built with Shopify
+        Liquid.
       </p>
       <img
         src={BookExcerpts}
@@ -72,8 +116,13 @@ function Projects() {
         alt="title page of book excerpts html page"
       />
       <p>
-      <a href="https://blood-in-my-eye-excerpts-html.vercel.app/" target="_blank" rel="noreferrer">{`{Book Excerpts}`}</a>{" "}
-      — A simple HTML project including excerpts from George Jackson's collection of letters and essays, Blood In My Eye.
+        <a
+          href="https://blood-in-my-eye-excerpts-html.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+        >{`{Book Excerpts}`}</a>{" "}
+        — A simple HTML project including excerpts from George Jackson's
+        collection of letters and essays, Blood In My Eye.
       </p>
     </div>
   );
